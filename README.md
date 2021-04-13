60735788326a53b305cc2ace204dd4b161fc0999
zhangjiamin <631839092@qq.com>
2021-04-13 17:20:16 +0800

    up

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mapbox_m_png
 mapbox转png图片
+
+## Build Setup
+
+``` bash
+# 安装依赖
+npm install
+
+# 启动页面，访问 localhost:6688
+npm run dev
+
+```
+
+## 修改自己的地图样式
+在static/config.json文件中修改，accessToken为自己的mapbox中的密钥，name为点击下载之后的图片名称，mapOp中的对象为mapbox的配置。
+其他的不需要改，如果需要改文件，修改src\components\mapbox下的Map.vue文件
---
+++ src/components/mapbox/Map.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id='map'>
+    <export-map :map=map></export-map>
+  </div>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+import exportMap from '@/components/export-map/Export'
+
+export default {
+  name: 'Map',
+  components: {
+    exportMap
+  },
+  data () {
+    return {
+      map: null
+    }
+  },
+  mounted () {
+    this.initMap()
+  },
+  methods: {
+    initMap () {
+      let that = this
+      let config = that.$baseConfig
+      mapboxgl.accessToken = config.accessToken
+      let optio = config.mapOp[0]
+      let map = new mapboxgl.Map(optio)
+      map.addControl(new mapboxgl.NavigationControl(), 'top-left')
+      that.map = map
+    }
+  }
+}
+</script>
+
+<style scoped>
+#map {
+  height: 100vh;
+  width: 100vw;
+}
+</style>>
